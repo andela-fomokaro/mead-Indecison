0e471c900617300745eff05b664d2dd0a3f1f35d
andela-fomokaro <faith.omokaro@andela.com>
2018-07-16 08:22:01 +0100

    working with react event handlers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,58 +1,71 @@
 "use strict";
 
-var user = {
-  name: "Omokaro Faith",
-  age: 25,
-  location: "Lagos, Nigeria"
-};
-
+console.log('app is running');
 var app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of a computer",
-  options: ['One', 'Two']
+  options: []
 };
 
-var getLocation = function getLocation(location) {
-  return location ? React.createElement(
-    "p",
-    null,
-    "Location: ",
-    location
-  ) : "";
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    renderApp();
+  }
 };
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h2",
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  app.options.length > 0 ? "Here are your options" : "No options"
-);
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  renderApp();
+};
 
-var templateTwo = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
+var renderApp = function renderApp() {
+  var template = React.createElement(
+    "div",
     null,
-    user.name
-  ),
-  React.createElement(
-    "p",
-    null,
-    "Age: ",
-    user.age
-  ),
-  getLocation(user.location)
-);
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "p",
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "button",
+      { onClick: onRemoveAll },
+      "Remove All"
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add Option"
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
 var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+renderApp();
--- src/app.js
+++ src/app.js
@@ -1,34 +1,43 @@
-const user = {
-  name: "Omokaro Faith",
-  age: 25,
-  location: "Lagos, Nigeria"
-};
-
+console.log('app is running');
 const app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of a computer",
-  options: ['One', 'Two']
+  options: []
 };
 
-const getLocation = ( (location) => {
-  return (location ? <p>Location: {location}</p> : "");
-})
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
-const template = (
-  <div>
-    <h2>{app.title}</h2>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    {app.options.length > 0 ? "Here are your options" : "No options"}
-  </div>
-);
+  let option = e.target.elements.option.value;
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    renderApp();
+  }
+}
 
-const templateTwo = (
-  <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    {getLocation(user.location)}
-  </div>
-)
+const onRemoveAll = () => {
+  app.options = [];
+  renderApp();
+}
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  
+  ReactDOM.render(template, appRoot);
+}
+
 const appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+renderApp();
