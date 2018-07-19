4cf7fd096a4eb0aace1f1d0bb04aa159f3b969f7
andela-fomokaro <faith.omokaro@andela.com>
2018-07-19 14:36:12 +0100

    turn class based components to stateless components

--- src/app.js
+++ src/app.js
@@ -57,48 +57,46 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1> {this.props.title} </h1>
-        <h2> {this.props.subtitle} </h2>
-      </div>
-    )
-  }
+// Stateless Components
+const Header = (props) => {
+  return (
+    <div>
+      <h1> {props.title} </h1>
+      <h2> {props.subtitle} </h2>
+    </div>
+  )
 }
 
-class Action extends React.Component {
-  render () {
-    return (
-      <div>
-        <button disabled={!this.props.hasOptions} onClick={this.props.handlePick}> What should I do? </button>
-      </div>
-    )
-  }
+// Stateless Component
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+        disabled={!props.hasOptions}
+        onClick={props.handlePick}> What should I do? </button>
+    </div>
+  )
 }
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}> Remove All </button>
-        {
-          this.props.options.map( (option) => <Option key={option} optionText={option} />)
-        }
-      </div>
-    )
-  }
+// Stateless components
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}> Remove All </button>
+      {
+        props.options.map( (option) => <Option key={option} optionText={option} />)
+      }
+    </div>
+  )
 }
 
-class Option extends React.Component {
-  render() {
-    return (
-      <div>
-       {this.props.optionText}
-      </div>
-    )
-  }
+// Stateless Components
+const Option = (props) => {
+  return (
+    <div>
+     {props.optionText}
+    </div>
+  )
 }
 
 class AddOption extends React.Component {
