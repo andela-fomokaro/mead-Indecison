0c9e26050b6cb2b35d57b2b292ecc42bb854a591
andela-fomokaro <faith.omokaro@andela.com>
2018-07-25 10:11:37 +0100

    add onRequestClose method for closing modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,6 +5,7 @@ const OptionModal = (props) => (
   <Modal
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
+    onRequestClose={props.handleClearSelectedOption}
   >
     <h3>Selected Option</h3>
     {props.selectedOption && <p>{props.selectedOption}</p>}
